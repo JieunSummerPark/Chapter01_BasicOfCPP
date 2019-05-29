78250a5dbb83f3fab1f31e00ce16cd1a29c54cf5
JieunSummerPark <parkj2@student.swosu.edu>
2019-05-30 01:00:03 +0900

    Game Over Game Version 2

--- GameOver/GameOverMain.cpp
+++ GameOver/GameOverMain.cpp
@@ -1,7 +1,7 @@
 // GameOverMain.cpp
 #include <stdio.h>
 
-void main()
+void merong()
 {
 	printf("Game Over!\n");
 }
---
+++ GameOver/GameOverV2.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main()
+{
+	printf("자꾸 오류남. 안 할래.");
+}
